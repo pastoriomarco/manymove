4b63b56b1e858c786c7f52dee24f7812f4da5541
pastoriomarco <pastoriomarco@gmail.com>
2025-03-06 17:54:34 +0100

    added standalone Panda robot examples with moveitcpp and movegroup (and renamed some old launchers)

--- manymove_planner/launch/panda_moveitcpp_fake_cpp_trees.launch.py
+++ manymove_planner/launch/panda_moveitcpp_fake_cpp_trees.launch.py
@@ -1,8 +1,10 @@
-from launch.actions import OpaqueFunction, DeclareLaunchArgument
+import os
+from launch.actions import OpaqueFunction, DeclareLaunchArgument, ExecuteProcess
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
+from ament_index_python.packages import get_package_share_directory
 
 def launch_setup(context, *args, **kwargs):
 
@@ -35,6 +37,7 @@ def launch_setup(context, *args, **kwargs):
         .planning_pipelines(
             pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
         )
+        .moveit_cpp(file_path=get_package_share_directory("manymove_planner") + "/config/moveit_cpp.yaml")
         .to_moveit_configs()
     )
 
@@ -48,7 +51,7 @@ def launch_setup(context, *args, **kwargs):
             moveit_configs.to_dict(),
             {
                 'node_prefix': "{}_".format(planning_group.perform(context)),
-                'planner_type': "movegroup",
+                'planner_type': "moveitcpp",
                 
                 'planning_group': planning_group,
                 'base_frame': base_frame,
@@ -57,6 +60,73 @@ def launch_setup(context, *args, **kwargs):
             }
         ],
     )
+
+
+    # RViz
+    rviz_config_file = (
+        get_package_share_directory("moveit2_tutorials") + "/launch/move_group.rviz"
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            moveit_configs.robot_description,
+            moveit_configs.robot_description_semantic,
+            moveit_configs.robot_description_kinematics,
+            moveit_configs.planning_pipelines,
+            moveit_configs.joint_limits,
+        ],
+    )
+
+    # Static TF
+    static_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "panda_link0"],
+    )
+
+        # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[moveit_configs.robot_description],
+    )
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("moveit_resources_panda_moveit_config"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[moveit_configs.robot_description, ros2_controllers_path],
+        output="both",
+    )
+
+    # Load controllers
+    load_controllers = []
+    for controller in [
+        "panda_arm_controller",
+        "panda_hand_controller",
+        "joint_state_broadcaster",
+    ]:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=["ros2 run controller_manager spawner {}".format(controller)],
+                shell=True,
+                output="screen",
+            )
+        ]
+
 
     # ================================================================
     # launch manymove_object_manager
@@ -108,15 +178,18 @@ def launch_setup(context, *args, **kwargs):
 
     return [
         ros2_control_hardware_type,
+        rviz_node,
+        static_tf,
+        robot_state_publisher,
         action_server_node,
+        ros2_control_node,
         object_manager_node,
         manymove_hmi_node,
         manymove_cpp_trees_node
-    ]
+    ] + load_controllers
 
 def generate_launch_description():
     return LaunchDescription([
-        
         # DeclareLaunchArguments for planning_group, base_frame, tcp_frame
         DeclareLaunchArgument('planning_group', default_value='panda_arm', description='MoveIt planning group'),
         DeclareLaunchArgument('base_frame', default_value='panda_link0', description='Base frame of the robot'),
@@ -128,3 +201,5 @@ def generate_launch_description():
         # OpaqueFunction to set up the node
         OpaqueFunction(function=launch_setup)
     ])
+
+
