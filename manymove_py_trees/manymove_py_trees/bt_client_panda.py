4201c02fef563b547fbc3d1965eb4cf4b9ccd92f
Marco Pastorio <pastoriomarco@gmail.com>
2025-03-15 10:38:39 +0100

    updated bt_client_panda with now move action tipe

--- manymove_py_trees/manymove_py_trees/bt_client_panda.py
+++ manymove_py_trees/manymove_py_trees/bt_client_panda.py
@@ -6,15 +6,16 @@ from rclpy.node import Node
 import py_trees
 import py_trees_ros
 
+import time
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 from manymove_py_trees.move_definitions import (
     define_movement_configs,
     create_move,
 )
-from manymove_py_trees.tree_helper import create_tree_from_sequences, create_tree_from_sequence, create_tree_from_sequence_v2 
-
-import time
+from manymove_py_trees.tree_helper import create_tree_from_sequences
+# Removed non-existent imports:
+# from manymove_py_trees.tree_helper import create_tree_from_sequence, create_tree_from_sequence_v2
 
 def main():
     rclpy.init()
@@ -26,86 +27,49 @@ def main():
 
     # 2) Define move sets
     joint_rest = [0.0, -0.785, 0.0, -2.355, 0.0, 3.14, 0.785]
-    # joint_look_sx = [0, -0.419, 1.378, 0.349, 1.535, -0.977, 0.0]
-    # joint_look_dx = [0.733, -0.297, 1.378, -0.576, 1.692, 1.291, 0.0]
-
+    # (If you want more scanning moves, define them here)
     named_home = "ready"
 
     pick_target = Pose(
         position=Point(x=0.3, y=0.3, z=0.25),
         orientation=Quaternion(x=1.0, y=0.0, z=0.0, w=0.0)
     )
-
     approach_target = Pose(
         position=Point(x=pick_target.position.x, y=pick_target.position.y, z=pick_target.position.z + 0.02),
         orientation=Quaternion(x=1.0, y=0.0, z=0.0, w=0.0)
     )
 
-    # Define move sequences:
-
-    # First, a single move: in the create_tree_from_sequence function, it will be planned alone and will take less time to process
-    # The next sequence of moves will be then be planned while the first is executing
+    # Define your sequences of moves
     rest_position = [
         create_move("joint", joint_values=joint_rest, config=movement_configs["max_move"]),
     ]
-
-    # # This sequence scans the surroundings to build the octomap
-    # scan_surroundings = [
-    #     #create_move("joint", joint_values=joint_rest, config=movement_configs["mid_move"]),
-    #     create_move("joint", joint_values=joint_look_sx, config=movement_configs["max_move"]),
-    #     create_move("joint", joint_values=joint_look_dx, config=movement_configs["max_move"]),
-    # ]
-
-    # This sequence simulates a series of moves typical when picking up an object, minus the commands to action the gripper that are under construction
     pick_sequence = [
         create_move("pose", target=approach_target, config=movement_configs["mid_move"]),
         create_move("cartesian", target=pick_target, config=movement_configs["slow_move"]),
         create_move("cartesian", target=approach_target, config=movement_configs["max_move"]),
     ]
-
     home_position = [
         create_move("named", named_target=named_home, config=movement_configs["max_move"]),
     ]
 
-    # whole_sequence = [
-    #     create_move("joint", joint_values=joint_rest, config=movement_configs["max_move"]),
-    #     create_move("joint", joint_values=joint_look_sx, config=movement_configs["max_move"]),
-    #     create_move("joint", joint_values=joint_look_dx, config=movement_configs["max_move"]),
-    #     create_move("pose", target=approach_target, config=movement_configs["mid_move"]),
-    #     create_move("cartesian", target=pick_target, config=movement_configs["slow_move"]),
-    #     create_move("cartesian", target=approach_target, config=movement_configs["max_move"]),
-    #     create_move("named", named_target=named_home, config=movement_configs["max_move"]),
-    # ]
-
-    # 3) Build the tree with our parallel plan/exec logic
+    # For demonstration, we'll chain these sequences in order
     list_of_sequences = [rest_position, pick_sequence, home_position]
-    # this chained branch executes faster: it leverages a more parallel computation of the following moves since the parallelization in python is not perfect
-    chained_branch = create_tree_from_sequences(node, list_of_sequences, root_name="LogicSequence")
 
-    # # Here I define sequence per sequence to test other ways of building the sequence. This results slower than create_tree_from_sequences
-    # rest_branch= create_tree_from_sequence(node, rest_position, root_name="Rest_Sequence")
-    # scan_branch= create_tree_from_sequence(node, scan_surroundings, root_name="Scan_Sequence")
-    # pick_branch= create_tree_from_sequence(node, pick_sequence, root_name="Pick_Sequence")
-    # home_branch= create_tree_from_sequence(node, home_position, root_name="Home_Sequence")
+    # 3) Build the Behavior Tree from these sequences
+    chained_branch = create_tree_from_sequences(
+        node,
+        list_of_sequences,
+        root_name="LogicSequence"
+    )
 
-    # # This also results slower than create_tree_from_sequences:
-    # whole_seq_branch= create_tree_from_sequence_v2(node, whole_sequence, root_name="Whole_Sequence")
-
-    main_seq = py_trees.composites.Sequence("Main_Sequence", True)
+    main_seq = py_trees.composites.Sequence("Main_Sequence", memory=True)
     main_seq.add_child(chained_branch.root)
 
-    # # I can then add other branches to test
-    # main_seq.add_child(rest_branch.root)
-    # main_seq.add_child(scan_branch.root)
-    # main_seq.add_child(pick_branch.root)
-    # main_seq.add_child(home_branch.root)
-    # main_seq.add_child(whole_seq_branch.root)
-
-    # This decorator will repeat the sequence indefinitely, for test purposes
+    # Optionally, decorate with a "Repeat forever" for test loops
     repeated_root = py_trees.decorators.Repeat(
-    child=main_seq,
-    num_success=-1,   # means repeat indefinitely
-    name="RepeatForever"
+        child=main_seq,
+        num_success=-1,  # infinite repeat
+        name="RepeatForever"
     )
 
     bt_tree = py_trees_ros.trees.BehaviourTree(repeated_root)
