f9cd40c5408807b110e15f9438dd6508419de1a7
pastoriomarco <pastoriomarco@gmail.com>
2025-10-10 21:45:13 +0200

    blackened pyton files

--- manymove_cpp_trees/launch/bt_lite_fake.launch.py
+++ manymove_cpp_trees/launch/bt_lite_fake.launch.py
@@ -11,28 +11,30 @@ def generate_launch_description():
     return LaunchDescription(
         [
             DeclareLaunchArgument(
-                'robot_model', default_value='lite6', description='Model of the robot (e.g., lite6, uf850, xarm)'
+                "robot_model", default_value="lite6", description="Model of the robot (e.g., lite6, uf850, xarm)"
             ),
             DeclareLaunchArgument(
-                'robot_prefix', default_value='', description='Prefix for the name of the robot arms'
+                "robot_prefix", default_value="", description="Prefix for the name of the robot arms"
             ),
-            DeclareLaunchArgument('tcp_frame', default_value='link_tcp', description='Name of the link to attach/detach objects to/from'),
             DeclareLaunchArgument(
-                'is_robot_real',
-                default_value='false',
-                description='Set to true if connected with a real robot exposing the necessary services needed by manymove_signals',
+                "tcp_frame", default_value="link_tcp", description="Name of the link to attach/detach objects to/from"
+            ),
+            DeclareLaunchArgument(
+                "is_robot_real",
+                default_value="false",
+                description="Set to true if connected with a real robot exposing the necessary services needed by manymove_signals",
             ),
             Node(
-                package='manymove_cpp_trees',
-                executable='bt_client',
-                name='manymove_cpp_trees_single_robot',
-                output='screen',
+                package="manymove_cpp_trees",
+                executable="bt_client",
+                name="manymove_cpp_trees_single_robot",
+                output="screen",
                 parameters=[
                     {
-                        'robot_model': LaunchConfiguration('robot_model'),
-                        'robot_prefix': LaunchConfiguration('robot_prefix'),
-                        'tcp_frame': LaunchConfiguration('tcp_frame'),
-                        'is_robot_real': LaunchConfiguration('is_robot_real'),
+                        "robot_model": LaunchConfiguration("robot_model"),
+                        "robot_prefix": LaunchConfiguration("robot_prefix"),
+                        "tcp_frame": LaunchConfiguration("tcp_frame"),
+                        "is_robot_real": LaunchConfiguration("is_robot_real"),
                     }
                 ],
             ),
